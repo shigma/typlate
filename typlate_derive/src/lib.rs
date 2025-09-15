ec887b3fce6c0498d53cdc715eee8d2f80584957
Shigma <shigma10826@gmail.com>
2025-09-15 22:47:19 +0800

    feat: support unnamed fields

--- typlate_derive/src/lib.rs
+++ typlate_derive/src/lib.rs
@@ -1,45 +1,44 @@
-use proc_macro::{Span, TokenStream};
-use quote::{format_ident, quote};
-use syn::{Data, DeriveInput, Fields, LitStr, parse_macro_input};
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::spanned::Spanned;
+use syn::{Data, DeriveInput, Fields, LitStr, Member, parse_macro_input};
 
 #[proc_macro_derive(TemplateParams)]
 pub fn derive_template_params(input: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(input as DeriveInput);
+    let input: DeriveInput = parse_macro_input!(input);
     let ident = &input.ident;
 
-    let fields = match &input.data {
+    let mut ident_names = vec![];
+    let mut match_arms = vec![];
+
+    match &input.data {
         Data::Struct(data) => match &data.fields {
-            Fields::Named(fields) => fields
-                .named
-                .iter()
-                .map(|f| f.ident.clone().unwrap())
-                .collect::<Vec<_>>(),
-            Fields::Unnamed(fields) => fields
-                .unnamed
-                .iter()
-                .enumerate()
-                .map(|(i, _)| format_ident!("{}", i))
-                .collect::<Vec<_>>(),
-            Fields::Unit => vec![],
+            Fields::Named(fields) => {
+                for (index, field) in fields.named.iter().enumerate() {
+                    let ident = field.ident.as_ref().unwrap();
+                    ident_names.push(LitStr::new(&ident.to_string(), field.span()));
+                    match_arms.push(quote! { #index => self.#ident.to_string(), });
+                }
+            }
+            Fields::Unnamed(fields) => {
+                for (index, field) in fields.unnamed.iter().enumerate() {
+                    let member = Member::Unnamed(index.into());
+                    ident_names.push(LitStr::new(&index.to_string(), field.span()));
+                    match_arms.push(quote! { #index => self.#member.to_string(), });
+                }
+            }
+            Fields::Unit => {}
         },
         _ => panic!("TemplateParams can only be derived for structs"),
-    };
-
-    let field_names = fields
-        .iter()
-        .map(|field| LitStr::new(&field.to_string(), Span::call_site().into()));
-
-    let field_matches = fields.iter().enumerate().map(|(index, field)| {
-        quote! { #index => self.#field.to_string(), }
-    });
+    }
 
     quote! {
         impl TemplateParams for #ident {
-            const FIELDS: &'static [&'static str] = &[#(#field_names),*];
+            const FIELDS: &'static [&'static str] = &[#(#ident_names),*];
 
             fn get_field(&self, index: usize) -> String {
                 match index {
-                    #(#field_matches)*
+                    #(#match_arms)*
                     _ => panic!("Index out of bounds"),
                 }
             }
